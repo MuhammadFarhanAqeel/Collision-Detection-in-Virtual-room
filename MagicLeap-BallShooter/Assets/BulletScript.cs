fc4ad3fc2b90550c3ee5f1b21c78cf7503f7e5b6
farhan <farhan.aqeel@hotmail.com>
2018-04-20 23:26:52 +0500

    Magic leap collision detection with virtual room
    Magic leap collision detection with virtual room
    
    Complete video : https://youtu.be/zhuK68dBCvk

---
+++ MagicLeap-BallShooter/Assets/BulletScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+
+
+
+
+    public GameObject Particles;
+    bool _collisionDone;
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!_collisionDone)
+        {
+            Debug.Log(collision.gameObject.name);
+            Instantiate(Particles, collision.contacts[0].point, transform.rotation);
+            _collisionDone = true;
+        }
+    }
+}
